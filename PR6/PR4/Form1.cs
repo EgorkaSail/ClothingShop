349876a63340b795d31b442bfee2dbcf6e2f923f
EgorkaSail <115783018+EgorkaSail@users.noreply.github.com>
2023-02-12 23:06:24 +0700

    PR6
    pr6

---
+++ PR6/PR4/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 newForm = new Form2();
+            newForm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Form3 newForm = new Form3();
+            newForm.Show();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Form4 newForm = new Form4();
+            newForm.Show();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            Form5 newForm = new Form5();
+            newForm.Show();
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            Form6 newForm = new Form6();
+            newForm.Show();
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+
+            Form7 newForm = new Form7();
+            newForm.Show();
+        }
+    }
+}
